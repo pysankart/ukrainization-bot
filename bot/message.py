8b00003d5f60b0c01d1546136fa260908e319b9b
lymagics <viacheslav.lymanskyi@nure.ua>
2022-09-30 04:15:08 +0300

    Add message edit translate.

--- bot/message.py
+++ bot/message.py
@@ -13,15 +13,16 @@ provider.register_destination(UkrainianLanguage)
 
 def message_handler(update: Update, context: CallbackContext) -> None:
     """Telegram bot message handler."""
-    text = str(update.message.text)
+    message = update.message or update.edited_message
+    text = str(message.text)
     translated = provider.translate(text)
 
     if text != translated:
-        from_user = update.message.forward_from or update.message.from_user 
+        from_user = message.forward_from or message.from_user 
         author = f"@{from_user.username}" if from_user.username else str(from_user.first_name)
 
-        chat_id = update.message.chat_id
-        message_id = update.message.message_id
+        chat_id = message.chat_id
+        message_id = message.message_id
 
         delete_message(context, chat_id, message_id)
         send_message(context, chat_id, author, translated)
